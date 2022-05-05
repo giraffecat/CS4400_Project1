e8ed6217f4411ab21321765c73bb2972048dee40
hui <1018997102@qq.com>
2022-05-05 00:05:03 -0400

    deposit withdraw

--- backend/router.js
+++ backend/router.js
@@ -2,12 +2,15 @@
 
 // 引入express并且获取路由对象
 const app = require('express')
+const moment = require('moment')
+
 const router = app.Router()
 var mysql      = require('mysql');
+
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password: 'kd971119', // mysql用户密码
+    host: 'localhost', // 服务器地址
+    user: 'root', // mysql用户名称
+    password: 'zh1998501', // mysql用户密码
     port: '3306', // 端口
     database: 'bank_management', // 数据
   });
@@ -127,6 +130,19 @@ router.get('/GetAccounts',(req,res)=>{
     })
 })
 
+router.post('/GetAccounts',(req,res)=>{
+    let query = `select accountID from access where perID = "${req.body.LoginPerson}";`;
+    let promise = new Promise(function(resolve, reject) {
+        db.query(query, [], function (results, fields) {
+            // 以json的形式返回
+            //判断是不是admin
+            resolve(results)
+        })
+    }).then(data => {
+        res.json(data)
+    })
+})
+
 router.get('/GetCustomers',(req,res)=>{
     let query = `select * from customer;`;
     let promise = new Promise(function(resolve, reject) {
@@ -140,8 +156,22 @@ router.get('/GetCustomers',(req,res)=>{
     })
 })
 
-router.get('/GetBanks',(req,res)=>{
-    let query = `select bankID from bank;`;
+// router.get('/GetBanks',(req,res)=>{
+//     let query = `select bankID from bank;`;
+//     let promise = new Promise(function(resolve, reject) {
+//         db.query(query, [], function (results, fields) {
+//             // 以json的形式返回
+//             //判断是不是admin
+//             resolve(results)
+//         })
+//     }).then(data => {
+//         res.json(data)
+//     })
+// })
+
+router.post('/GetBanks',(req,res)=>{
+    console.log("account",req.body.LoginPerson);
+    let query = `select bankID from access where perID = "${req.body.LoginPerson}";`;
     let promise = new Promise(function(resolve, reject) {
         db.query(query, [], function (results, fields) {
             // 以json的形式返回
@@ -153,6 +183,45 @@ router.get('/GetBanks',(req,res)=>{
     })
 })
 
+
+
+router.post('/Deposit',(req,res)=>{
+    console.log("account",req.body);
+    var promise = new Promise(function(resolve, reject){
+        let time = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+        let query = `call account_deposit("${req.body.PersonID}", ${req.body.Amount}, "${req.body.BankID}", "${req.body.AccountID}", "${time}");`;        
+        connection.query(query, function (err, result) {
+        if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+        }        
+        data = result
+        resolve(data)  
+        // res.end(JSON.stringify(data));
+        });
+    }).then(data => {
+        res.end(JSON.stringify(data));
+    })
+})
+
+router.post('/Withdraw',(req,res)=>{
+    console.log("account",req.body);
+    var promise = new Promise(function(resolve, reject){
+        let time = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+        let query = `call account_withdrawal("${req.body.PersonID}", ${req.body.Amount}, "${req.body.BankID}", "${req.body.AccountID}", "${time}");`;        
+        connection.query(query, function (err, result) {
+        if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+        }        
+        data = result
+        resolve(data)  
+        // res.end(JSON.stringify(data));
+        });
+    }).then(data => {
+        res.end(JSON.stringify(data));
+    })
+})
 
 router.get('/register',(req,res)=>{
     res.send('注册')
--- frontend/phase4-frontend/src/views/Deposit.vue
+++ frontend/phase4-frontend/src/views/Deposit.vue
@@ -27,7 +27,7 @@
       </div>
       <div class="menu-item" >
         <el-button @click="back" class="btn" type="primary">Back</el-button>
-        <el-button class="btn" type="primary">Deposit</el-button>
+        <el-button class="btn" type="primary" @click = "Deposit">Deposit</el-button>
       </div>
     </div>
   </div>
@@ -59,8 +59,11 @@ export default {
     },
     GetBanks:function() {
       this.axios({
-      method: "get",
+      method: "post",
       url: "http://localhost:3000/GetBanks", // 接口地址
+      data:{
+        LoginPerson: this.global.LoginPerson
+      }
       }).then(res => {
         console.log("bankID",res)
         this.form.bankID = res.data
@@ -68,15 +71,41 @@ export default {
     },
     GetAccounts: function() {
       this.axios({
-      method: "get",
+      method: "post",
       url: "http://localhost:3000/GetAccounts", // 接口地址
+      data:{
+        LoginPerson: this.global.LoginPerson
+      }
       }).then(res => {
         console.log("account",res.data)
         this.form.accountID = res.data
       })
     },
     show:function(){
-      console.log(this.global.httpUrl);
+      console.log("asd",this.global.LoginPerson);
+    },
+    Deposit:function() {
+      console.log("this.global.LoginPerson",this.global.LoginPerson)
+      if(this.Amount && this.selectedBank && this.selectedAccount) {
+        this.axios({
+          method: "post",
+          url: "http://localhost:3000/Deposit", // 接口地址
+          data:{
+            PersonID : this.global.LoginPerson,
+            Amount : this.Amount,
+            BankID: this.selectedBank,
+            AccountID: this.selectedAccount
+          }
+          }).then(res => {
+            // console.log("account",res.data)
+            // this.form.accountID = res.data
+          })
+      }else {
+        this.$message({
+          message: `Please check your select`,
+          type: 'warning'
+        });
+      }
     }
   }
 }
