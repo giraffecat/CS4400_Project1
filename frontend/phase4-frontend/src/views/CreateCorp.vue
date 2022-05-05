ac709aacdc4b293ebc7159e1b2b666065e5785f7
jluo331 <jluo1119@gmail.com>
2022-05-05 10:34:25 +0800

    corporation

--- frontend/phase4-frontend/src/views/CreateCorp.vue
+++ frontend/phase4-frontend/src/views/CreateCorp.vue
@@ -5,24 +5,24 @@
         Create Corporation
       </div>
       <div class="corp-form" >
-        <el-form ref="form" :model="form" label-width="150px">
-        <el-form-item label="Corporation ID" :rules="[{required:true, message:'Please input a valid corporation ID!'}]">
-          <el-input v-model="form.corpID"></el-input>
+        <el-form ref="corpform" :model="corpform" label-width="120px">
+        <el-form-item label="Corporation ID" prop="corpID" :rules="[{required:true, message:'Please input a corporation ID!'}]">
+          <el-input v-model="corpform.corpID"></el-input>
         </el-form-item>
-        <el-form-item label="Long Name" required>
-          <el-input v-model="form.corpLN" :rules="[{required:true, message:'Please input a valid corporation long name!'}]"></el-input>
+        <el-form-item label="Long Name" prop="corpLN" :rules="[{required:true, message:'Please input a long name!'}]">
+          <el-input v-model="corpform.corpLN"></el-input>
         </el-form-item>
-        <el-form-item label="Short Name" required>
-          <el-input v-model="form.corpSN" :rules="[{required:true, message:'Please input a valid corporation short name!'}]"></el-input>
+        <el-form-item label="Short Name" prop="corpSN" :rules="[{required:true, message:'Please input a short name!'}]">
+          <el-input v-model="corpform.corpSN"></el-input>
         </el-form-item>
         <el-form-item label="Reserved Assets">
-          <el-input v-model="form.corpAssets"></el-input>
+          <el-input v-model="corpform.corpAssets"></el-input>
         </el-form-item>
         </el-form>
 
       </div>
       <div class="buttons-item" >
-        <el-button class="btn" type="primary" @click="onCreate">Create</el-button>
+        <el-button class="btn" type="primary" @click="submitForm('corpform')">Create</el-button>
         <el-button class="btn" type="primary" @click="onCancel">Cancel</el-button>
       </div>
     </div>
@@ -32,7 +32,7 @@
 export default {
     data(){
       return{
-        form: {
+        corpform: {
             corpID: '',
             corpLN: '',
             corpSN: '',
@@ -41,43 +41,38 @@ export default {
       }
     },
     methods: {
-      onCreate() {
-        console.log("create Corp",this.corpID, this.corpLN, this.corpSN, this.corpAssets)
-        this.axios({
-        method: "post",
-        url: "http://localhost:3000/createCorp", // 接口地址
-        data: {
-            corpID: this.corpID,   // 传接口参数
-            corpLN: this.corpLN,
-            corpSN: this.corpSN,
-            corpAssets: this.corpAssets
-            }
-        }).then(response => {
-            // console.log(response.data[0].role);
-            if(response.data.length != 0) {
-            this.$message({
-                message: 'Sucessfully login!',
+      submitForm(formName){
+        this.$refs[formName].validate((valid)=>{
+          if(valid){
+            // if(this.corpform.corpAssets == ''){
+            //   this.corpform.corpAssets = 0
+            // }
+            console.log(this.corpform)
+            this.axios({
+              method:"post",
+              url: "http://localhost:3000/createCorp", // 接口地址
+              data:{
+                corpID: this.corpform.corpID,   // 传接口参数
+                corpLN: this.corpform.corpLN,
+                corpSN: this.corpform.corpSN,
+                corpAssets: this.corpform.corpAssets == '' ? 0:this.console.corpAssets
+              }
+            }).then(response=>{
+              this.$refs[formName].resetFields();
+                this.$message({
+                message: 'Sucessfully submit!',
                 type: 'success'
-            });
-            if(response.data[0].role=='customer'){
-                this.$router.push('/customermenu')
-            }
-            if(response.data[0].role=='admin'){
-                this.$router.push('/adminmenu')
-            }
-            if(response.data[0].role=='manager'){
-                this.$router.push('/managermenu')
-            }
-            } else {
-            this.$message({
-                message: 'Check your accountID and password!',
-                type: 'warning'
-            });
-            }   // 成功的返回      
-        }).catch(error => console.log(error, "error")); // 失败的返回
+                });
+            }).catch(error => console.log(error, "error")); // 失败的返回
+          } else{
+            console.log("error");
+            return false
+          }
+        })
       },
       onCancel(){
-    console.log('cancel!')
+        console.log('cancel!')
+        this.$router.push('/adminmenu')
       }
     }
 }
