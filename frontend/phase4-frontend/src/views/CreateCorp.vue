da6a4a901a5fb1112f1db85588623b7f47e71990
jluo331 <jluo1119@gmail.com>
2022-05-05 08:57:03 +0800

    revise createcorp

--- frontend/phase4-frontend/src/views/CreateCorp.vue
+++ frontend/phase4-frontend/src/views/CreateCorp.vue
@@ -6,17 +6,17 @@
       </div>
       <div class="corp-form" >
         <el-form ref="form" :model="form" label-width="150px">
-        <el-form-item label="Corporation ID">
-          <el-input v-model="form.ID"></el-input>
+        <el-form-item label="Corporation ID" :rules="[{required:true, message:'Please input a valid corporation ID!'}]">
+          <el-input v-model="form.corpID"></el-input>
         </el-form-item>
-        <el-form-item label="Long Name">
-          <el-input v-model="form.LN"></el-input>
+        <el-form-item label="Long Name" required>
+          <el-input v-model="form.corpLN" :rules="[{required:true, message:'Please input a valid corporation long name!'}]"></el-input>
         </el-form-item>
-        <el-form-item label="Short Name">
-          <el-input v-model="form.SN"></el-input>
+        <el-form-item label="Short Name" required>
+          <el-input v-model="form.corpSN" :rules="[{required:true, message:'Please input a valid corporation short name!'}]"></el-input>
         </el-form-item>
         <el-form-item label="Reserved Assets">
-          <el-input v-model="form.Assets"></el-input>
+          <el-input v-model="form.corpAssets"></el-input>
         </el-form-item>
         </el-form>
 
@@ -33,16 +33,48 @@ export default {
     data(){
       return{
         form: {
-            ID: '',
-            LN: '',
-            SN: '',
-            Assets: ''
+            corpID: '',
+            corpLN: '',
+            corpSN: '',
+            corpAssets: ''
         }
       }
     },
     methods: {
       onCreate() {
-        console.log('submit!');
+        console.log("create Corp",this.corpID, this.corpLN, this.corpSN, this.corpAssets)
+        this.axios({
+        method: "post",
+        url: "http://localhost:3000/createCorp", // 接口地址
+        data: {
+            corpID: this.corpID,   // 传接口参数
+            corpLN: this.corpLN,
+            corpSN: this.corpSN,
+            corpAssets: this.corpAssets
+            }
+        }).then(response => {
+            // console.log(response.data[0].role);
+            if(response.data.length != 0) {
+            this.$message({
+                message: 'Sucessfully login!',
+                type: 'success'
+            });
+            if(response.data[0].role=='customer'){
+                this.$router.push('/customermenu')
+            }
+            if(response.data[0].role=='admin'){
+                this.$router.push('/adminmenu')
+            }
+            if(response.data[0].role=='manager'){
+                this.$router.push('/managermenu')
+            }
+            } else {
+            this.$message({
+                message: 'Check your accountID and password!',
+                type: 'warning'
+            });
+            }   // 成功的返回      
+        }).catch(error => console.log(error, "error")); // 失败的返回
       },
       onCancel(){
     console.log('cancel!')
